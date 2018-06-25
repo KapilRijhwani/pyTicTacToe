1e8edc803e6f9c8e292a057998866b5b2c11dd5d
kapil-x-rijhwani <kapil.rijhwani@oracle.com>
2018-06-26 01:30:36 +0530

    1. Checking for victory and draw.

--- game.py
+++ game.py
@@ -12,12 +12,56 @@ def print_board():
     print()
 
 
-
-while True:
-    print_board()
+def player_move(icon):
+    if icon == "X":
+        number = 1
+    elif icon == "O":
+        number = 2
+    print("Your turn player {}.".format(number))
     choice = int(input("Enter your move(1 - 9): ").strip())
     if board[choice - 1] == " ":
-        board[choice - 1] = 'X'
+        board[choice - 1] = icon
     else:
         print()
         print("Choice is taken")
+
+
+def is_victory(icon):
+    if (board[0] == icon and board[1] == icon and board[2] == icon) or \
+            (board[3] == icon and board[4] == icon and board[5] == icon) or \
+            (board[6] == icon and board[7] == icon and board[8] == icon) or \
+            (board[0] == icon and board[3] == icon and board[6] == icon) or \
+            (board[1] == icon and board[4] == icon and board[7] == icon) or \
+            (board[2] == icon and board[5] == icon and board[8] == icon) or \
+            (board[0] == icon and board[4] == icon and board[8] == icon) or \
+            (board[2] == icon and board[4] == icon and board[6] == icon):
+        return True
+    else:
+        return False
+
+
+def is_draw():
+    if " " not in board:
+        return True
+    else:
+        return False
+
+
+while True:
+    print_board()
+    player_move("X")
+    print_board()
+    if is_victory("X"):
+        print("X Wins.. Congratulations ! GAME OVER")
+        break
+    elif is_draw():
+        print("It's a draw. GAME OVER")
+        break
+    player_move("O")
+    if is_victory("O"):
+        print_board()
+        print("O Wins.. Congratulations ! GAME OVER")
+        break
+    elif is_draw():
+        print("It's a draw. GAME OVER")
+        break
